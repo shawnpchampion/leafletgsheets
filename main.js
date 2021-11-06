f3728be4edd608ff116d2a2a56740980181ad4ac
shawnpchampion <shawnpchampion@gmail.com>
2021-11-06 02:50:37 -1000

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function init() {
           zoom: 16,
           attributionControl: false,
           tap: false,
-          dragging: false,
+//          dragging: false,
 //          dragging: !L.Browser.mobile,
 //          tap: !L.Browser.mobile,
           zoomControl: false
